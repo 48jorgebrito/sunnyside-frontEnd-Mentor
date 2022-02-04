9c5fcda2706767a5c27075af23fe83a488d1f915
48jorgebrito <jorgeramirobrito19@gmail.com>
2022-02-04 16:25:24 -0300

    repositorio sunnyside

---
+++ index.js
@@ -0,0 +1,9 @@
+let mobile = document.querySelector(".box-menu-mobile")
+let btnMobile = document.querySelector(".fa-bars")
+
+btnMobile.addEventListener("click", function(){
+    mobile.classList.toggle("menu-mobile-active")
+})
+
+
+
